a3c1673af4d2ad7d61d9acbd842878446a5b94b0
wbogatyrewa <veronika01-01@mail.ru>
2023-06-09 01:22:09 +0500

    add tests to the first task

--- DutchFlagsProblem/dutchFlagProblem.test.ts
+++ DutchFlagsProblem/dutchFlagProblem.test.ts
@@ -1,5 +1,16 @@
 import dutchFlagProblem from "./dutchFlagProblem";
 
-test("sort string array [\"green\", \"blue\", \"red\", \"red\"]", () => {
-    expect(dutchFlagProblem(["green", "blue", "red", "red"])).toStrictEqual(["red", "red", "green", "blue"]);
+describe("test for Dutch Flags Problem", () => {
+    test("sort string array [\"green\", \"blue\", \"red\", \"red\"]", () => {
+        expect(dutchFlagProblem(["green", "blue", "red", "red"])).toStrictEqual(["red", "red", "green", "blue"]);
+    });
+    test("sort string array [\"green\", \"green\", \"red\", \"red\"]", () => {
+        expect(dutchFlagProblem(["green", "green", "red", "red"])).toStrictEqual(["red", "red", "green", "green"]);
+    });
+    test("sort string array [\"green\"]", () => {
+        expect(dutchFlagProblem(["green"])).toStrictEqual(["green"]);
+    });
+    test("sort null array []", () => {
+        expect(dutchFlagProblem([])).toStrictEqual([]);
+    });
 });
